b0649e7d5661c76e41563413504c30688075250d
sufyaldi <sufyaldys@gmail.com>
2023-07-28 14:09:28 +0800

    challenge#1_sufyaldi

---
+++ challenge#1.js
@@ -0,0 +1,13 @@
+
+function sum() {
+  let total = 0;
+  for (let i=0; i < arguments.length; i++) {
+    total += arguments[i];
+  }
+return total;
+}
+
+console.log(sum(1, 2, 7)); 
+console.log(sum(1, 4));    
+console.log(sum(11));      
+console.log(sum(10, 3, 6, 7, 9)); 
