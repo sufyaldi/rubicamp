61845118abaeb35c29c5890114508e2e0531532e
sufyaldi <sufyaldys@gmail.com>
2023-08-02 05:56:54 +0800

    Manipulasi String

--- challenge#5.js
+++ challenge#5.js
@@ -2,12 +2,12 @@ function stringManipulation(word) {
     const hrfVkl = ['a', 'e', 'i', 'o', 'u'];
     const hrfPrtm = word[0].toLowerCase();
   
-    if (hrfVkl.includes(hrfPertama)) {
+    if (hrfVkl.includes(hrfPrtm)) {
       console.log(word); 
     } else {
       console.log(word.slice(1) + hrfPrtm + 'nyo'); 
     }
   }
-  
+
 stringManipulation('ayam');
 stringManipulation('bebek')
