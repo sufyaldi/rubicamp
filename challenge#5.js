a185b5dc2ce74cbd3189f5fc6565d9ce116dbfa0
sufyaldi <sufyaldys@gmail.com>
2023-08-03 22:31:10 +0800

    Manipulasi Kalimat

--- challenge#5.js
+++ challenge#5.js
@@ -1,27 +1,13 @@
-// function stringManipulation(word) {
-//     const hrfVkl = ['a', 'e', 'i', 'o', 'u'];
-//     const hrfPrtm = word[0].toLowerCase();
-  
-//     if (hrfVkl.includes(hrfPrtm)) {
-//       console.log(word); 
-//     } else {
-//       console.log(word.slice(1) + word[0] + 'nyo'); 
-//     }
-//   }
-
-// stringManipulation('ayam');
-// stringManipulation('bebek')
-
 function stringManipulation(word) {
-  const hrfVkl = ['a', 'e', 'i', 'o', 'u'];
-  const hrfPrtm = word[0]; // Hapus toLowerCase() agar huruf pertama tetap huruf besar
+    const hrfVkl = ['a', 'e', 'i', 'o', 'u'];
+    const hrfPrtm = word[0].toLowerCase();
   
-  if (hrfVkl.includes(hrfPrtm.toLowerCase())) {
-    console.log(word);
-  } else {
-    console.log(word.slice(1) + hrfPrtm.toLowerCase() + 'nyo'); // Gunakan toLowerCase() di sini agar kata pertama tetap huruf kecil
+    if (hrfVkl.includes(hrfPrtm)) {
+      console.log(word); 
+    } else {
+      console.log(word.slice(1) + word[0] + 'nyo'); 
+    }
   }
-}
 
 stringManipulation('Ayam');
-stringManipulation('Bebek');
+stringManipulation('Bebek')
