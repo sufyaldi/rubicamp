bbb659d9250af8bdc9a626c54d927daf953e5caa
sufyaldi <sufyaldys@gmail.com>
2023-08-03 20:11:02 +0800

    Weird Multiply

--- challenge#7.js
+++ challenge#7.js
@@ -2,11 +2,9 @@ function weirdMultiply(sentence){
     if (sentence <= 9) {
         return sentence;
       }
-    
-      var angkaString = sentence.toString();
+          var angkaString = sentence.toString();
       var result = 1;
-    
-      for (let i = 0; i < angkaString.length; i++) {
+          for (let i = 0; i < angkaString.length; i++) {
         let digit = parseInt(angkaString[i]);
         result *= digit;
       }
